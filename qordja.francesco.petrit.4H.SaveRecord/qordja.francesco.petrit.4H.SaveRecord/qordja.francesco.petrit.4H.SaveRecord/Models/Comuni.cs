d6e1af35ff97c3a6d40f11e43266dee793c690e2
frank0456 <77667178+frank0456@users.noreply.github.com>
2021-05-26 15:06:58 +0200

    Add files via upload

---
+++ qordja.francesco.petrit.4H.SaveRecord/qordja.francesco.petrit.4H.SaveRecord/qordja.francesco.petrit.4H.SaveRecord/Models/Comuni.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+    public int ID { get; set; }
+    private string _nomeComune;
+    public string NomeComune
+    {
+        get => _nomeComune;
+
+        set
+        {
+           
+            if (value.Length == 22)
+                _nomeComune = value;
+
+            if (value.Length < 22)
+                value = value.PadRight(22);
+            else if (value.Length > 22)
+                value = value.Substring(0, 22);
+
+            _nomeComune = value;
+        }
+    }
+    private string _codiceCatastale;
+
+    public string CodiceCatastale
+    {
+        get => _codiceCatastale;
+
+        set
+        {
+            if (value.Length == 4)
+                _codiceCatastale = value;
+
+            if (value.Length < 4)
+                value = value.PadRight(4);
+            else if (value.Length > 4)
+                value = value.Substring(0, 4);
+
+            _codiceCatastale = value;
+        }
+    }
+
+    public Comune() { }
+
+    public Comune(string riga, int id)
+    {
+        string[] colonne = riga.Split(',');
+        ID = id;
+        CodiceCatastale = colonne[0];
+        NomeComune = colonne[1];
+    }
+
+    public override string ToString() => $"{ID}, {CodiceCatastale}, {NomeComune}";
+}
+
+public class Comuni : List<Comune> 
+{
+    public string NomeFile { get; }
+
+    public Comuni() { }
+
+    public Comuni(string fileName)
+    {
+        NomeFile = fileName;
+
+        using (FileStream fin = new FileStream(fileName, FileMode.Open))
+        {
+            StreamReader reader = new StreamReader(fin);
+
+            int id = 1;
+
+            while (!reader.EndOfStream)
+            {
+                string riga = reader.ReadLine();
+                Comune c = new Comune(riga, id++);
+                Add(c);
+            }
+        }
+    }
+
+    public void Save()
+    {
+        string fileName = NomeFile.Split('.')[0] + ".bin";
+        Save(fileName);
+    }
+
+    public void Save(string fileName)
+    {
+        using (FileStream fout = new FileStream(fileName, FileMode.Create))
+        {
+            BinaryWriter writer = new BinaryWriter(fout);
+
+            foreach (Comune comune in this)
+            {
+                writer.Write(comune.ID);
+                writer.Write(comune.CodiceCatastale);
+                writer.Write(comune.NomeComune);
+            }
+            writer.Flush();
+            writer.Dispose();
+        }
+    }
+
+    public void Load()
+    {
+        string fn = NomeFile.Split('.')[0] + ".bin";
+        Load(fn);
+    }
+
+    public void Load(string fileName)
+    {
+        Clear(); 
+
+        using (FileStream fin = new FileStream(fileName, FileMode.Open))
+        {
+            BinaryReader reader = new BinaryReader(fin);
+            Comune c = new Comune();
+
+            while (reader.BaseStream.Position != reader.BaseStream.Length)
+            {
+                // Leggo l'ID
+                c.ID = reader.ReadInt32();
+                // Leggo il codice catastale
+                c.CodiceCatastale = reader.ReadString();
+                // Leggo il nome del comune
+                c.NomeComune = reader.ReadString();
+                Add(c);
+            }
+        }
+    }
+
+    public Comune RicercaComune(int index)
+    {
+        string fn = NomeFile.Split('.')[0] + ".bin";
+        return RicercaComune(index, fn);
+    }
+
+    
+    public Comune RicercaComune(int index, string fileName)
+    {
+        FileStream fin = new FileStream(fileName, FileMode.Open);
+        BinaryReader reader = new BinaryReader(fin);
+
+        fin.Seek((index - 1) * 32, SeekOrigin.Begin);
+        Comune c = new Comune();
+        c.ID = reader.ReadInt32();
+        c.CodiceCatastale = reader.ReadString();
+        c.NomeComune = reader.ReadString();
+
+        return c;
+    }
+
+	
+}
---
+++ qordja.francesco.petrit.4H.SaveRecord/qordja.francesco.petrit.4H.SaveRecord/qordja.francesco.petrit.4H.SaveRecord/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace qordja.francesco.petrit._4H.SaveRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int verifica = 8;
+            Console.WriteLine("SaveRecord");
+            
+            
+            // Leggo un file CSV con i comuni e lo trasformo in una list<Comune>
+            Comuni c = new Comuni( "Comuni.csv" );
+            Console.WriteLine($"Ho letto {c.Count} righe...");
+
+            Console.WriteLine($"Ecco la riga {verifica}: {c[verifica]}");
+
+            
+            // Scrivo la List<Comune> in un file binario
+            c.Save();
+            
+            
+            // Rileggere il file binario in una List<Comune>
+            c.Load();
+            Console.WriteLine($"Ho letto {c.Count} righe dal file binario...");
+        
+            Console.WriteLine($"Ecco la riga {verifica}: {c[verifica]}");
+        }
+    }
+}
